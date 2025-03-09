eebe02b160e7b775068bfca4569fd6fe80b52bbb
samktan <samktan@gmail.com>
2025-03-09 13:41:26 +1100

    tried to rewrite as functions, didn't go well. but managed to consolidate MOS into a single script.

--- scripts/ssh-wrapper.sh
+++ scripts/ssh-wrapper.sh
@@ -1,50 +1,38 @@
-#!/bin/bash
-
-# Name: sssh*
+# Sam's SSH (SSSH)
+# SSH wrapper that prompts to delete invalid known hosts keys
 # Author: sam.k.tan@oracle.com
 
-ssh () {
-# save the input parameters
-PARAMS="$*"
+sssh () {
+	# save the input parameters
+	PARAMS="$*"
 
-# get the paths to the executables
-SSH=$(which ssh)
-SSHKEYGEN=$(which ssh-keygen)
-SSSH="::::"
+	# get the paths to the executables
+	SSH="/usr/bin/ssh"
+	SSHKEYGEN="/usr/bin/ssh-keygen"
+	SSSH="::::"
 
-echo "${SSSH} SSH Wrapper by sam.k.tan@oracle.com"
+	echo "${SSSH} Sam's SSH by sam.k.tan@oracle.com"
 
-# run SSH with the given parameters
-${SSH} ${PARAMS}
+	# run SSH with the given parameters
+	${SSH} ${PARAMS}
+	# if SSH did not exit with status code 255 ...
+	RC=$? && [ ${R} -ne 255 ] && return ${RC}
 
-# if SSH exits with status code 255 ...
-if [ $? -eq 255 ]; then
 	# extract the hostname from the SSH parameters
-	HOST=$(${SSH} -G ${PARAMS} | grep -i '^hostname' | cut -d ' '  -f2)
-
+	HOST=$(${SSH} -G ${PARAMS} | grep -i '^hostname' | cut -d ' ' -f2)
 	# check if hostname exists in known_hosts file
 	${SSHKEYGEN} -F ${HOST} > /dev/null 2>&1
-
 	# if hostname does not exist, then exit
-	if [ $? -eq 1 ]; then
-		exit 0
-	fi
+	[ $? -ne 0 ] && return ${RC}
 
 	# since hostname exists, ask to delete and try again
 	echo "${SSSH} Matching hostname found in known_hosts file."
 	echo "${SSSH} Do you want to remove the offending host key(s) and retry the connection?"
 	echo "${SSSH} Only YES is accepted, any other key to cancel."
-	read RESPONSE
+	read R
 
-	if [ "xx${RESPONSE}xx" == "xxYESxx" ]; then
-		echo "${SSSH} Removing offending keys ..."
-		${SSHKEYGEN} -R ${HOST}
-
-		echo "${SSSH} Retrying SSH ..."
-		${SSH} ${PARAMS}
-	else
-		echo "${SSSH} Exiting ... no changes made."
-	fi
-fi
+	[ "xx${R}xx" != "xxYESxx" ] && return ${RC}
+	${SSHKEYGEN} -R ${HOST}
+	${SSH} ${PARAMS}
 }
-
+alias ssh=sssh
